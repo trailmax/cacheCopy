b187c2336c3dbe95a0d49c483c37cf05d5fcbb8e
Max Vasilyev <trailmax@users.noreply.github.com>
2012-01-23 23:59:37 +0000

    IE problem on XP

--- Browsers/IEHelper.cs
+++ Browsers/IEHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using SYS = System.Environment;
 
 namespace cacheCopy
 {
@@ -33,32 +34,39 @@ namespace cacheCopy
         /// <returns></returns>
         public List<ProfilePath> getProfiles()
         {
-            string userProfile = System.Environment.GetEnvironmentVariable("USERPROFILE");
-            string localAppData = System.Environment.GetEnvironmentVariable("LOCALAPPDATA");
+            string userProfile = SYS.GetEnvironmentVariable("USERPROFILE");
+            string localAppData = SYS.GetEnvironmentVariable("LOCALAPPDATA");
 
             // list of possible paths with the temp.
             // added in the order of possibility
-            String[] paths = new String[]
+            List<String> paths = new List<String>()
             {
                 Util.ReadRegistryKey(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\", "Cache"),
                 Util.ReadRegistryKey(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\", "Cache"),
-                Path.Combine(localAppData, @"Microsoft\Windows\Temporary Internet Files\Low\Content.IE5"),
-                Path.Combine(userProfile, @"Local Settings\Temp\Temporary Internet Files\Content.IE5"),
-                Path.Combine(userProfile, @"Local Settings\Temporary Internet Files"),
-                Path.Combine(userProfile, @"Local Settings\Temp\Temporary Internet Files"),
-                Path.Combine(userProfile, @"Local Settings\Temporary Internet Files"),
+                localAppData + @"\Microsoft\Windows\Temporary Internet Files\Low\Content.IE5",
+                userProfile + @"\Local Settings\Temp\Temporary Internet Files\Content.IE5",
+                userProfile + @"\Local Settings\Temporary Internet Files",
+                userProfile + @"\Local Settings\Temp\Temporary Internet Files",
+                userProfile + @"\Local Settings\Temporary Internet Files",
             };
 
-            foreach (string path in paths)
+            paths.RemoveAll(d => !Directory.Exists(d));
+
+            String profile = "";
+            if (paths.Any())
             {
-                // if the directory exists, use it as a default single profile
-                if (Directory.Exists(path))
-                {
-                    return new List<ProfilePath> { new ProfilePath("Internet Explorer", path) };
-                }
+		        profile = paths.First();
+            }
+	        else
+            {
+                return new List<ProfilePath>();
             }
 
-            return new List<ProfilePath>();
+
+            return new List<ProfilePath>(){ 
+                new ProfilePath("Internet Explorer", profile)
+            };
+
         }
 
 
