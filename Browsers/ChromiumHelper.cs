e61301fbc9ad9bf14a12f03966791936392a7a90
trailmax <trailmax1@gmail.com>
2012-01-28 19:54:11 +0000

    Added support for chrome and chromium

---
+++ Browsers/ChromiumHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SYS = System.Environment;
+
+
+namespace cacheCopy
+{
+    /// <summary>
+    /// Helper class to help detect cache paths for Chromium
+    /// </summary>
+    class ChromiumHelper : DefaultBrowser, IBrowserHelper
+    {
+
+        protected override List<string>  GetListOfPossiblePaths()
+        {
+            List<String> list = new List<String> {
+                SYS.GetEnvironmentVariable("USERPROFILE") + @"\Local Settings\Application Data\Chromium\User Data\Default\Cache",
+                SYS.GetEnvironmentVariable("USERPROFILE") + @"\AppData\Local\Chromium\User Data\Default",
+            };
+
+            return list;
+        }
+
+        protected override string  GetBrowserName()
+        {
+            return "Chromium";
+        }
+
+    }
+}
