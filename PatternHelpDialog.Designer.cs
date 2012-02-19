c6cb0748ff5f2371ccf084760bc1102349472927
trailmax <trailmax1@gmail.com>
2012-02-19 22:22:46 +0500

    crap attempt to use datarepeater for help dialog

--- PatternHelpDialog.Designer.cs
+++ PatternHelpDialog.Designer.cs
@@ -31,8 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatternHelpDialog));
             this.txtHelpDescription = new System.Windows.Forms.RichTextBox();
             this.btnClose = new System.Windows.Forms.Button();
-            this.dataGrid = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.dataRepeater1 = new Microsoft.VisualBasic.PowerPacks.DataRepeater();
+            this.txtExplanation = new System.Windows.Forms.TextBox();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.btnCopyPattern = new System.Windows.Forms.Button();
+            this.dataRepeater1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtHelpDescription
@@ -50,7 +53,7 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(270, 397);
+            this.btnClose.Location = new System.Drawing.Point(293, 410);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 1;
@@ -58,16 +61,42 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // dataGrid
+            // dataRepeater1
             // 
-            this.dataGrid.AllowUserToAddRows = false;
-            this.dataGrid.AllowUserToDeleteRows = false;
-            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(12, 114);
-            this.dataGrid.Name = "dataGrid";
-            this.dataGrid.ReadOnly = true;
-            this.dataGrid.Size = new System.Drawing.Size(356, 265);
-            this.dataGrid.TabIndex = 2;
+            // 
+            // dataRepeater1.ItemTemplate
+            // 
+            this.dataRepeater1.ItemTemplate.Size = new System.Drawing.Size(348, 33);
+            this.dataRepeater1.Location = new System.Drawing.Point(12, 184);
+            this.dataRepeater1.Name = "dataRepeater1";
+            this.dataRepeater1.Size = new System.Drawing.Size(356, 220);
+            this.dataRepeater1.TabIndex = 2;
+            this.dataRepeater1.Text = "dataRepeater1";
+            // 
+            // txtExplanation
+            // 
+            this.txtExplanation.Location = new System.Drawing.Point(145, 114);
+            this.txtExplanation.Name = "txtExplanation";
+            this.txtExplanation.ReadOnly = true;
+            this.txtExplanation.Size = new System.Drawing.Size(209, 20);
+            this.txtExplanation.TabIndex = 2;
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(72, 116);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.ReadOnly = true;
+            this.txtPattern.Size = new System.Drawing.Size(67, 20);
+            this.txtPattern.TabIndex = 1;
+            // 
+            // btnCopyPattern
+            // 
+            this.btnCopyPattern.Location = new System.Drawing.Point(12, 114);
+            this.btnCopyPattern.Name = "btnCopyPattern";
+            this.btnCopyPattern.Size = new System.Drawing.Size(54, 23);
+            this.btnCopyPattern.TabIndex = 0;
+            this.btnCopyPattern.Text = "Copy";
+            this.btnCopyPattern.UseVisualStyleBackColor = true;
             // 
             // PatternHelpDialog
             // 
@@ -75,16 +104,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(380, 445);
-            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.txtExplanation);
+            this.Controls.Add(this.dataRepeater1);
+            this.Controls.Add(this.txtPattern);
             this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCopyPattern);
             this.Controls.Add(this.txtHelpDescription);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PatternHelpDialog";
             this.Text = "File Naming Pattern Help";
+            this.Load += new System.EventHandler(this.PatternHelpDialog_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PatternHelpDialog_KeyUp);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PatternHelpDialog_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.dataRepeater1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,6 +126,9 @@
 
         private System.Windows.Forms.RichTextBox txtHelpDescription;
         private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.DataGridView dataGrid;
+        private Microsoft.VisualBasic.PowerPacks.DataRepeater dataRepeater1;
+        private System.Windows.Forms.TextBox txtExplanation;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Button btnCopyPattern;
     }
 }
