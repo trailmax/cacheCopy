b27121a9163d0fea9c4fa8ba0705463ad8b3df1c
trailmax <trailmax1@gmail.com>
2012-03-04 09:44:19 +0500

    unit test for Updater

---
+++ cacheCopyTests/UpdaterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cacheCopy;
+using NUnit.Framework;
+using NUnit.Mocks;
+
+namespace cacheCopyTests
+{
+    [TestFixture]
+    class UpdaterTest
+    {
+
+        private DynamicMock gui;
+
+
+        [Test]
+        public void testUpdater()
+        {
+            
+            Updater updater = new Updater(null);
+
+        }
+    }
+}
